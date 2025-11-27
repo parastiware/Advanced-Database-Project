8dadcb2afac21cabb172877485c587832fde6422
Paras Tiwari <parastiware@gmail.com>
2025-11-27 12:11:44 -0600

    Add Frontend service

---
+++ services/frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <div>
+            <h1>Polyglot Persistence Demo</h1>
+            <p>Frontend is running!</p>
+        </div>
+    </React.StrictMode>
+);
